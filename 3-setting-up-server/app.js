82733cd4f9c252da176699fec78d8703b592ba2d
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-03-15 13:00:05 +0500

    SEND HTML RESPONSE

--- 3-setting-up-server/app.js
+++ 3-setting-up-server/app.js
@@ -1,7 +1,13 @@
 const http = require("http");
 
 function rqListener(req, res) {
-  console.log(req);
+  console.log(req.url, req.method);
+  //   console.log(res);
+  //   process.exit();
+  //   res.setHeader("Content-Type", "application/json");
+  res.setHeader("Content-Type", "text/html");
+  res.write("<h1>hello</h1>");
+  res.end();
 }
 
 const server = http.createServer(rqListener);
