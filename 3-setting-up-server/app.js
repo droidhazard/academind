43089b7953e3c607ce9f5a31ca2e1cf0e76d5b22
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-03-15 22:58:16 +0500

    return html form

--- 3-setting-up-server/app.js
+++ 3-setting-up-server/app.js
@@ -1,12 +1,21 @@
 const http = require("http");
 
 function rqListener(req, res) {
-  console.log(req.url, req.method);
+  // console.log(req.url, req.method);
+  const url = req.url;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST" name="message"><input type="text"><button type="submit">Send</button></form></body>',
+    );
+    res.write("</html>");
+    return res.end();
+  }
   //   console.log(res);
   //   process.exit();
-  //   res.setHeader("Content-Type", "application/json");
-  res.setHeader("Content-Type", "text/html");
-  res.write("<h1>hello</h1>");
+  // res.setHeader("Content-Type", "application/json");
   res.end();
 }
 
