08846d64a8d4390c8ab86816873dcbaab069ec5b
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-03-16 05:38:36 +0500

    parse request body

--- 3-setting-up-server/app.js
+++ 3-setting-up-server/app.js
@@ -10,14 +10,25 @@ function rqListener(req, res) {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
     res.write(
-      '<body><form action="/message" method="POST" name="message"><input type="text"><button type="submit">Send</button></form></body>',
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>',
     );
     res.write("</html>");
     return res.end();
   }
 
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("data.txt", "DUMMY");
+    const body = [];
+
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      console.log(message);
+      fs.writeFileSync("data.txt", message);
+    });
     res.statusCode = 302;
     res.setHeader("Location", "/");
     return res.end();
