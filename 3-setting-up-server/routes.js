12466538c7e1ed1979e0c668ceec9a76db55950f
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-03-17 19:28:41 +0500

    solve runtime error

--- 3-setting-up-server/routes.js
+++ 3-setting-up-server/routes.js
@@ -4,7 +4,7 @@ const requestHandler = (req, res) => {
   const url = req.url;
   const method = req.method;
   if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
+    // res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
     res.write(
@@ -21,7 +21,7 @@ const requestHandler = (req, res) => {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on("end", () => {
+    return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
       console.log(message);
@@ -32,9 +32,14 @@ const requestHandler = (req, res) => {
         return res.end();
       });
     });
-
-    // console.log(req.body);
   }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title><head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
 };
 
 // ^ METHOD 1
