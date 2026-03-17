7b225b02dbe8bb364aeeff3bb21afcc87f30564a
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-03-17 10:27:04 +0500

    install nodemon

--- 3-setting-up-server/app.js
+++ 3-setting-up-server/app.js
@@ -1,48 +1,11 @@
 const http = require("http");
-const fs = require("fs");
 
-function rqListener(req, res) {
-  // console.log(req.url, req.method);
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>',
-    );
-    res.write("</html>");
-    return res.end();
-  }
+const routes = require("./routes");
 
-  if (url === "/message" && method === "POST") {
-    const body = [];
+// function rqListener(req, res) {
+//   // console.log(req.url, req.method);
+// }
 
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      console.log(message);
-      // fs.writeFileSync("data.txt", message); // Blocks code execution until done
-      fs.writeFile("data.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-
-    // console.log(req.body);
-  }
-  //   console.log(res);
-  //   process.exit();
-  // res.setHeader("Content-Type", "application/json");
-  // res.end();
-}
-
-const server = http.createServer(rqListener);
+const server = http.createServer(routes.handler);
 
 server.listen(3333);
---
+++ 3-setting-up-server/routes.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>',
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      console.log(message);
+      // fs.writeFileSync("data.txt", message); // Blocks code execution until done
+      fs.writeFile("data.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+
+    // console.log(req.body);
+  }
+};
+
+// ^ METHOD 1
+module.exports = requestHandler;
+
+// ^ METHOD 2
+module.exports = {
+  handler: requestHandler,
+  someText: "Some Text",
+};
+
+// ^ METHOD 3
+module.exports.handler = requestHandler;
+module.exports.someText = "Some Text";
+
+// ^ METHOD 4
+exports.handler = requestHandler;
+exports.someText = "Some Text";
