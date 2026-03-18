a912ebd561f5fc75a430a5628e496f65361f99f4
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-03-18 22:30:53 +0500

    add account registration

--- 3-PRACTICE-login-system/routes.js
+++ 3-PRACTICE-login-system/routes.js
@@ -15,7 +15,7 @@ const requestHandler = (req, res) => {
         <a href="register"><button>Register</button></a>
       </body>
       </html>`);
-    res.end();
+    return res.end();
   }
 
   // * Register page for "/register"
@@ -34,7 +34,46 @@ const requestHandler = (req, res) => {
         <a href="login"><button>Login</button></a>
       </body>
       </html>`);
-    res.end();
+    return res.end();
+  }
+
+  // * Handle '/register' submission
+  if (url === "/register" && method === "POST") {
+    res.setHeader("Content-Type", "text/html");
+    const body = [];
+
+    req.on("data", (chunk) => {
+      // console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      let parsedBody = Buffer.concat(body).toString();
+      parsedBody = parsedBody.split("&").join("\n");
+      console.log(parsedBody);
+      fs.readFile("./database.txt", "utf-8", (err, data) => {
+        // console.log("OLD DATA start:", data, "\b:OLD DATA close");
+        fs.writeFile(
+          "./database.txt",
+          `${data}\n&&\n${parsedBody}`,
+          (err) => {},
+        );
+      });
+    });
+    res.write(`<html>
+      <head><title>Practice - Sign Up</title></head>
+      <body>
+        <h2>Welcome to practice</h2>
+        <form action="/register" method="POST">
+        <input type="text" name="username" placeholder="username"><br>
+        <input type="text" name="password" placeholder="password"><br>
+        <button type="submit">Register</button>
+        </form>
+        <br><hr>
+        <a href="login"><button>Login</button></a>
+      </body>
+      </html>`);
+    return res.end();
   }
 };
 
