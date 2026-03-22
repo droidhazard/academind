39696298fe079a1fcdb3f1c16a84ef1d4bd76e48
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-03-22 16:31:24 +0500

    add: path helper function for root directory

--- 4-working-with-expressjs/routes/shop.routes.js
+++ 4-working-with-expressjs/routes/shop.routes.js
@@ -1,18 +1,18 @@
 const path = require("path");
 const express = require("express");
 
+const rootDir = require("../utils/path");
+
 const router = express.Router();
 
 // * Server the homepage
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 // * Handle 404 page
 router.use((req, res) => {
-  res
-    .status(404)
-    .sendFile(path.join(__dirname, "../", "views", "page-not-found.html"));
+  res.status(404).sendFile(path.join(rootDir, "views", "page-not-found.html"));
 });
 
 module.exports = router;
