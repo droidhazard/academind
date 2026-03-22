e1a2191030094e97e9f452eabdbdee2d6e69f814
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-03-22 14:39:00 +0500

    create: html view pages

--- 4-working-with-expressjs/routes/admin.routes.js
+++ 4-working-with-expressjs/routes/admin.routes.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
+// * /admin/add-product => POST
 router.get("/add-product", (req, res, next) => {
   console.log("add-product");
   res.send(`<html><h1>The 'Add Product' Page</h1>
     <form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></html>`);
 });
 
+// * /admin/add-product => GET
 router.post("/add-product", (req, res) => {
   console.log(req.body);
   res.redirect("/");
