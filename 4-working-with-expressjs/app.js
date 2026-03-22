94442139d487a6fe45fe2a091f6c00116191bddf
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-03-22 10:06:39 +0500

    '/add-product' route, req.body parsing

--- 4-working-with-expressjs/app.js
+++ 4-working-with-expressjs/app.js
@@ -1,15 +1,23 @@
 const express = require("express");
+// const bodyParser = require("body-parser");
 
 const app = express();
+app.use(express.urlencoded());
+// app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  console.log("in the middleware");
-  next();
+app.use("/add-product", (req, res, next) => {
+  console.log("add-product");
+  res.send(`<html><h1>The 'Add Product' Page</h1>
+    <form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></html>`);
 });
 
-app.use((req, res, next) => {
+app.use("/product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   console.log("in ANOTHER middleware");
-  res.setHeader("Content-Type", "application/json");
   res.send("<h1>Hello from Express</h1>");
 });
 
