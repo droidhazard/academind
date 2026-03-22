faa8c8a73f8a487397b06440a50392c995c5ad31
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-03-22 11:51:49 +0500

    add 404 page

--- 4-working-with-expressjs/routes/shop.routes.js
+++ 4-working-with-expressjs/routes/shop.routes.js
@@ -7,4 +7,13 @@ router.get("/", (req, res, next) => {
   res.send("<h1>Hello from Express</h1>");
 });
 
+// * Handle 404 page
+router.use((req, res) => {
+  res
+    .status(404)
+    .send(
+      '<h2 style="color:white;background-color:maroon; padding: 2px 10px">404 ! Page not found</h2>',
+    );
+});
+
 module.exports = router;
