56ffcddb7c7710eac48c7db08469bf95bc606b54
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-03-23 07:47:58 +0500

    setup Basic express routing

--- 4-PRACTICE-dummy-crm/app.js
+++ 4-PRACTICE-dummy-crm/app.js
@@ -1,1 +1,8 @@
-console.log("hello");
+const express = require("express");
+app = express();
+
+const dashboardRoutes = require("./routes/dashboard.routes");
+
+app.use(dashboardRoutes);
+
+app.listen(3000);
