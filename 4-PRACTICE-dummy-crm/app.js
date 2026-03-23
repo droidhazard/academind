eff4cd4568e502936c498365bc7ee15c053b363d
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-03-23 08:53:41 +0500

    add: people and projects routes

--- 4-PRACTICE-dummy-crm/app.js
+++ 4-PRACTICE-dummy-crm/app.js
@@ -3,9 +3,13 @@ const path = require("path");
 app = express();
 
 const dashboardRoutes = require("./routes/dashboard.routes");
+const peopleRoutes = require("./routes/people.routes");
+const projectsRoutes = require("./routes/projects.routes");
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(dashboardRoutes);
+app.use(peopleRoutes);
+app.use(projectsRoutes);
 
 app.listen(3000);
