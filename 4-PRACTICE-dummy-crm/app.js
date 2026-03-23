f3172eda0a61aa818b47400f888db2043d863312
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-03-23 08:04:09 +0500

    server css file statically

--- 4-PRACTICE-dummy-crm/app.js
+++ 4-PRACTICE-dummy-crm/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const path = require("path");
 app = express();
 
 const dashboardRoutes = require("./routes/dashboard.routes");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(dashboardRoutes);
 
