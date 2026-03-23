73e8a945217373da317c18df6b8ad1969ba8d013
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-03-23 09:05:13 +0500

    add: 404 page

--- 4-PRACTICE-dummy-crm/app.js
+++ 4-PRACTICE-dummy-crm/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 app = express();
 
+const rootDir = require("./util/path");
+
 const dashboardRoutes = require("./routes/dashboard.routes");
 const peopleRoutes = require("./routes/people.routes");
 const projectsRoutes = require("./routes/projects.routes");
@@ -11,5 +13,10 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(dashboardRoutes);
 app.use(peopleRoutes);
 app.use(projectsRoutes);
+
+// * Handle 404 page
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+});
 
 app.listen(3000);
