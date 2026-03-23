f701f15831c8b49da1a7cdd7206ca4b5790a8ec0
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-03-23 10:46:28 +0500

    add: submission route

--- 4-PRACTICE-dummy-crm/routes/people.routes.js
+++ 4-PRACTICE-dummy-crm/routes/people.routes.js
@@ -4,8 +4,14 @@ const path = require("path");
 const router = express.Router();
 const rootDir = require("../util/path");
 
+// * '/people' page => GET
 router.get("/people", (req, res) => {
   res.sendFile(path.join(rootDir, "views", "people.html"));
 });
 
+// * 'people' create people submission => POST
+router.post("/people", (req, res) => {
+  console.log(req.body);
+  res.redirect("/people");
+});
 module.exports = router;
--- 4-PRACTICE-dummy-crm/routes/projects.routes.js
+++ 4-PRACTICE-dummy-crm/routes/projects.routes.js
@@ -4,8 +4,14 @@ const path = require("path");
 const router = express.Router();
 const rootDir = require("../util/path");
 
+// * '/projects' page => GET
 router.get("/projects", (req, res) => {
   res.sendFile(path.join(rootDir, "views", "projects.html"));
 });
 
+// * '/project' create project submissions => POST
+router.post("/projects", (req, res) => {
+  console.log(req.body);
+  res.redirect("/projects");
+});
 module.exports = router;
