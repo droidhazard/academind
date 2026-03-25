47e343c6e770deae0b441c51f3c483385d08ab5e
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-03-25 12:05:22 +0500

    fix: 'path' for 404 page

--- 4-PRACTICE-dummy-crm/app.js
+++ 4-PRACTICE-dummy-crm/app.js
@@ -23,7 +23,11 @@ app.use((req, res, next) => {
   // res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
   res
     .status(404)
-    .render("404.ejs", { pageTitle: "404 Not Found", path: "404" });
+    .render("404.ejs", {
+      pageTitle: "404 Not Found",
+      path: "404",
+      path: "egg",
+    });
 });
 
 app.listen(3000);
