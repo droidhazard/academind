8f5c62b41a551470986b16da9435996bf66769e9
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-03-26 08:20:20 +0500

    remove: shifted the 404 page

--- 4-working-with-expressjs/routes/shop.routes.js
+++ 4-working-with-expressjs/routes/shop.routes.js
@@ -10,10 +10,4 @@ const router = express.Router();
 // * Serve the homepage
 router.get("/", productsController.getProducts);
 
-// * Handle 404 page
-router.use((req, res) => {
-  // res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
-  res.status(404).render("404", { docTitle: "404 Not Found", path: "/404" });
-});
-
 module.exports = router;
