4b0adbae0e721710392cf63dbb47cafc07cc649a
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-04-01 10:06:56 +0500

    use project model

--- 4-PRACTICE-dummy-crm/controllers/projects.controller.js
+++ 4-PRACTICE-dummy-crm/controllers/projects.controller.js
@@ -1,17 +1,28 @@
-const projectsArray = [];
+// const projectsArray = [];
+
+const Project = require("../models/project.model");
 
 exports.getProjects = (req, res) => {
   // res.sendFile(path.join(rootDir, "views", "projects.html"));
+  const projects = Project.fetchAll();
   res.render("projects.ejs", {
     pageTitle: "Projects - Dummy CRM",
     path: "/projects",
-    projectsArray: projectsArray,
+    projectsArray: projects,
   });
 };
 
 exports.addProject = (req, res) => {
   // console.log(req.body);
-  projectsArray.push(req.body);
-  console.log(projectsArray);
+  // projectsArray.push(req.body);
+  // console.log(projectsArray);
+  const project = new Project(
+    req.body.title,
+    req.body.contact,
+    req.body.assigned,
+    req.body.priority,
+    req.body.duedate,
+  );
+  project.save();
   res.redirect("/projects");
 };
