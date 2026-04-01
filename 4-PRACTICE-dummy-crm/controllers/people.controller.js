4001bbb195e91be7b280c2093565964a4b7a3f36
Zohaib Ali <www.zohaib.0335@gmail.com>
2026-04-01 09:56:25 +0500

    use people model

--- 4-PRACTICE-dummy-crm/controllers/people.controller.js
+++ 4-PRACTICE-dummy-crm/controllers/people.controller.js
@@ -1,17 +1,26 @@
-const peopleArray = [];
+const People = require("../models/people.model");
 
 exports.getPeople = (req, res) => {
   // res.sendFile(path.join(rootDir, "views", "people.html"));
+  const people = People.fetchAll();
   res.render("people.ejs", {
     pageTitle: "People - Dummy CRM",
     path: "/people",
-    peopleArray: peopleArray,
+    peopleArray: people,
   });
 };
 
 exports.addPeople = (req, res) => {
   // console.log(req.body);
-  peopleArray.push(req.body);
+  // peopleArray.push(req.body);
+  const people = new People(
+    req.body.fname,
+    req.body.lname,
+    req.body.addr,
+    req.body.email,
+    req.body.phone,
+  );
+  people.save();
   // console.log(peopleArray);
   res.redirect("/people");
 };
